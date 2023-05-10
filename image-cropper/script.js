045704345bfd19ac9c78d0b26f5b1d96ea4d446a
Sabin Thapa <sabint017@gmail.com>
2023-05-10 13:59:32 +0545

    merged: UI with functionality

--- image-cropper/script.js
+++ image-cropper/script.js
@@ -7,15 +7,29 @@ let imageWidth = document.querySelector('.img-w')
 let options = document.querySelector('.options')
 let saveBtn = document.querySelector('.save')
 let downloadBtn = document.querySelector('.download')
+let nextBtn = document.querySelector('#next-btn')
+let prevBtn = document.querySelector('#prev-btn')
 
 let cropper = ""
 
+//List of files in the folder
+let fileList = []
+let currentIndex = 0;
 
 // After an image has been selected
 fileInput.addEventListener('change', e => {
-    console.log(e.target.files)
-    if (e.target?.files?.length) {
+    fileList = e.target.files;
+    showImage(0)
+
+})
+
+function showImage(index) {
+    if (fileList.length) {
         console.log('hey')
+
+        //First image
+        const file = fileList[index]
+        
         //To read the image
         const reader = new FileReader();
         reader.addEventListener('load', e => {
@@ -39,9 +53,9 @@ fileInput.addEventListener('change', e => {
                 cropper = new Cropper(img)
             }
         })
-        reader.readAsDataURL(e.target.files[0])
+        reader.readAsDataURL(file)
     }
-})
+}
 
 saveBtn.addEventListener('click', e => {
     e.preventDefault();
@@ -50,8 +64,25 @@ saveBtn.addEventListener('click', e => {
         width: imageWidth.value
     }).toDataURL();
 
-    croppedImage.classList.remove('hide')
-    downloadBtn.classList.remove('hide')
-    downloadBtn.download = 'imagename.png'
-    downloadBtn.setAttribute('href', imgSrc)
+    let downloadLink = document.createElement('a');
+    downloadLink.href = imgSrc;
+    downloadLink.download = `cropped_${currentIndex}.png`
+    document.body.appendChild(downloadLink)
+    downloadLink.click()
+    document.body.removeChild(downloadLink)
+
+    // croppedImage.classList.remove('hide')
+    // downloadBtn.classList.remove('hide')
+    // downloadBtn.download = 'imagename.png'
+    // downloadBtn.setAttribute('href', imgSrc)
+})
+
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % fileList.length;
+    showImage(currentIndex)
+})
+
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + fileList.length) % fileList.length;
+    showImage(currentIndex)
 })
