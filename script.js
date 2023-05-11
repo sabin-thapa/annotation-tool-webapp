8e82d17816e3357d8127658f5b989e0ee62b7967
Sabin Thapa <sabint017@gmail.com>
2023-05-11 12:51:03 +0545

    feat: complete scripts for selecting folders and performing cropping

--- script.js
+++ script.js
@@ -1,88 +1,133 @@
-let fileInput = document.querySelector('#file-input')
-let uploadedImage = document.querySelector('.result')
-let resultImage = document.querySelector('.img-result')
-let croppedImage = document.querySelector('.cropped')
-let imageWidth = document.querySelector('.img-w')
+let fileInput = document.querySelector("#file-input");
+let uploadedImage = document.querySelector(".result");
+let resultImage = document.querySelector(".img-result");
+let croppedImage = document.querySelector(".cropped");
+let imageWidth = document.querySelector(".img-w");
 
-let options = document.querySelector('.options')
-let saveBtn = document.querySelector('.save')
-let downloadBtn = document.querySelector('.download')
-let nextBtn = document.querySelector('#next-btn')
-let prevBtn = document.querySelector('#prev-btn')
+let options = document.querySelector(".options");
+let saveBtn = document.querySelector(".save");
+let downloadBtn = document.querySelector(".download");
+let nextBtn = document.querySelector("#next-btn");
+let prevBtn = document.querySelector("#prev-btn");
 
-let cropper = ""
+let cropper = "";
 
 //List of files in the folder
-let fileList = []
+let fileList = [];
 let currentIndex = 0;
 
-// After an image has been selected
-fileInput.addEventListener('change', e => {
-    fileList = e.target.files;
-    showImage(0)
+const saveCroppedEndpoint = `http://localhost:3000/save-cropped`;
+const saveOriginalEndpoint = `http://localhost:3000/save-original`;
 
-})
+// After an image has been selected
+fileInput.addEventListener("change", (e) => {
+  fileList = e.target.files;
+  showImage(0);
+});
 
 function showImage(index) {
-    if (fileList.length) {
-        console.log('hey')
+  if (fileList.length) {
+    console.log("hey");
 
-        //First image
-        const file = fileList[index]
-        
-        //To read the image
-        const reader = new FileReader();
-        reader.addEventListener('load', e => {
-            if(e.target.result) {
-                //Creating new image
-                let img = document.createElement('img');
-                img.id = 'image';
-                img.src = e.target.result;
+    //First image
+    const file = fileList[index];
 
-                //Clean uploaded image
-                uploadedImage.innerHTML = '';
+    //To read the image
+    const reader = new FileReader();
+    reader.addEventListener("load", (e) => {
+      if (e.target.result) {
+        //Creating new image
+        let img = document.createElement("img");
+        img.id = "image";
+        img.src = e.target.result;
 
-                //Append the new image
-                uploadedImage.appendChild(img);
+        //Clean uploaded image
+        uploadedImage.innerHTML = "";
 
-                //display buttons
-                saveBtn.classList.remove('hide')
-                options.classList.remove('hide')
+        //Append the new image
+        uploadedImage.appendChild(img);
 
-                //CROPPER
-                cropper = new Cropper(img)
-            }
-        })
-        reader.readAsDataURL(file)
-    }
+        //display buttons
+        saveBtn.classList.remove("hide");
+        options.classList.remove("hide");
+
+        //CROPPER
+        cropper = new Cropper(img);
+      }
+    });
+    reader.readAsDataURL(file);
+  }
 }
 
-saveBtn.addEventListener('click', e => {
+saveBtn.addEventListener("click", (e) => {
     e.preventDefault();
+  
+    // const img = document.querySelector(".result img");
 
-    let imgSrc = cropper.getCroppedCanvas({
-        width: imageWidth.value
-    }).toDataURL();
+    // Get cropped area of the image
+    const croppedCanvas = cropper.getCroppedCanvas();
+  
+    // Save cropped image
+    const croppedImageName = `cropped_${currentIndex}.png`;
+    const croppedImageLink = croppedCanvas.toDataURL("image/png");
+  
+    fetch(saveCroppedEndpoint, {
+      method: "POST",
+      body: JSON.stringify({
+        imageName: croppedImageName,
+        imageData: croppedImageLink,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          console.log("Cropped image saved successfully!");
+  
+          // Save original image
+          const originalImageName = `backup_${currentIndex}.png`;
+          const originalImageFile = fileList[currentIndex];
+  
+          const formData = new FormData();
+          formData.append("imageName", originalImageName);
+          formData.append("imageData", originalImageFile);
+  
+          fetch(saveOriginalEndpoint, {
+            method: "POST",
+            body: formData,
+          })
+            .then((res) => {
+              if (res.ok) {
+                console.log("Original image saved successfully!");
+              } else {
+                console.error("Failed to save original image!");
+              }
+              return res.text();
+            })
+            .then((data) => {
+              console.log(data, "Save original image response");
+            })
+            .catch((err) => {
+              console.error("Error saving original image: ", err);
+            });
+        } else {
+          console.error("Failed to save cropped image!");
+        }
+      })
+      .catch((err) => {
+        console.error("Error saving cropped image: ", err);
+      });
+      console.log("saved");
+  });
+  
+  
+nextBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % fileList.length;
+  showImage(currentIndex);
+});
 
-    let downloadLink = document.createElement('a');
-    downloadLink.href = imgSrc;
-    downloadLink.download = `cropped_${currentIndex}.png`
-    document.body.appendChild(downloadLink)
-    downloadLink.click()
-    document.body.removeChild(downloadLink)
-
-    // croppedImage.classList.remove('hide')
-    // downloadBtn.classList.remove('hide')
-    // downloadBtn.download = 'imagename.png'
-    // downloadBtn.setAttribute('href', imgSrc)
-})
-
-nextBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % fileList.length;
-    showImage(currentIndex)
-})
-
-prevBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + fileList.length) % fileList.length;
-    showImage(currentIndex)
-})
+prevBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + fileList.length) % fileList.length;
+  showImage(currentIndex);
+});
