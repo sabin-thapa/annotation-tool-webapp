c89358cfc42d6dccc7cb8dbbb9e874f970867dae
Sabin Thapa <sabint017@gmail.com>
2023-05-24 14:00:05 +0545

    fix: reload bug fix

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ const archiver = require("archiver");
 
 dotenv.config();
 
+app.use(express.static('public'));
+
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
